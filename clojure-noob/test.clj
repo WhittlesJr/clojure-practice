8f7c6b0fd59aa4b5bf5de213d770d0abf1cbdc63
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-06-12 12:25:47 -0400

    Chapter 3 progress

--- clojure-noob/test.clj
+++ clojure-noob/test.clj
@@ -113,3 +113,183 @@ failed-protagonist-names
 
 (:a mymap)
 (:x mymap "No gnome knows homes like Noah knows")
+
+(def myvec [0 1 2])
+
+(get myvec 2)
+
+(get ["a" {:name "Pugsley Winterbottom"} "c"] 1)
+
+(vector "creepy" "full" "moon")
+
+(conj myvec 3)
+
+'(1 2 3 4)
+
+(nth '(:a :b :c) 1)
+
+(list 1 "two" {3 4})
+
+(conj '(1 2 2 33) 4)
+
+#{"kurt vonnegut" 20 :icicle}
+
+(hash-set 1 1 2 2)
+
+(conj #{:a :b} :b)
+
+(set [3 3 3 4 4])
+
+(contains? #{:a :B} :a)
+
+(contains? #{:a :B} :3)
+
+(contains? #{nil} nil)
+
+(:a #{:a :b})
+
+(:c #{:a :b})
+
+(get #{:a :b} :a)
+
+(get #{:a nil} nil)
+
+(get #{:a nil} "kurt vonnegut")
+
+(+ 1 2 3 4)
+
+(first [1 2 3 4])
+
+(or + -)
+
+((or + -) 1 2 3)
+
+((and (= 1 1) +) 1 2 3)
+
+((first [+ 0]) 1 2 3)
+
+(inc 1.2)
+
+(map inc [0 1 2 3])
+
+(map inc '(0 1 2 3))
+
+(+ (inc 199) (/ 100 (- 7 2)))
+
+(+ 200 (/ 100 (- 7 2)))
+
+(defn too-enthusiastic
+  "Docstring"
+  [name]
+  (str "WHOA" name "STUFF"))
+
+(too-enthusiastic "Zelda" )
+
+(defn no-params []
+  "No params")
+
+(defn one-param [x]
+  (str "My one param is " x))
+
+(defn two-params [x y]
+  (str "Two params: " x y))
+
+(defn x-chop
+  "DO a chop"
+  ([name chop-type]
+   (str "I " chop-type " chop " name "! Take that!"))
+  ([name]
+   (x-chop name "karate")))
+
+(defn codger-communication
+  [whippersnapper]
+  (str "Get off my lawn, " whippersnapper))
+
+(defn codger
+  [& whippersnappers]
+  (map codger-communication whippersnappers))
+
+;; Return the first element of a collection
+(defn my-first
+  [[first-thing]]
+  first-thing)
+
+(my-first ["oven" "bike" "war-axe"])
+
+(defn chooser
+  [[first-choice second-choice & unimportant-choices]]
+  (println (str "First: " first-choice))
+  (println (str "Second: " second-choice))
+  (println (str "Rest: " (clojure.string/join ", " unimportant-choices))))
+
+(chooser ["Marmalade" "Handsome Jack" "Pigpen" "Aquaman"])
+
+
+(defn treasure-location
+  [{lat :lat long :long}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure long: " long)))
+
+(treasure-location {:lat 28.22 :long 81.33})
+
+(str nil)
+
+
+(defn treasure-location-2
+  [{:keys [lat long]}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure long: " long)))
+
+(treasure-location-2 {:lat 28.22 :long 81.33})
+
+
+(defn treasure-location-3
+  [{:keys [lat long] :as treasure-location}]
+  (println (str "Treasure lat: " lat))
+  (println (str "Treasure long: " long))
+  (println treasure-location))
+
+(treasure-location-3 {:lat 28.22 :long 81.33})
+
+(defn dosomestuff []
+  (+ 1 301)
+  30
+  "joe")
+
+(defn bignums [x]
+  (if (> x 6)
+    "So big!"
+    "bleh"))
+
+(bignums 5)
+
+(bignums 7)
+
+(fn [params]
+  (println params))
+
+(map (fn [name] (str "Hi, " name))
+     ["Darth Vader" "Mr. Magoo" "Doggie" "Mark" "Lisa" ])
+
+((fn [x] (* x 3)) 8)
+
+(def my-special-multiplier (fn [x] (* x 3)))
+(my-special-multiplier 15)
+
+(#(* % 3) 8)
+
+(map  #(str "Hi, " %)
+     ["Darth Vader" "Mr. Magoo" "Doggie" "Mark" "Lisa" ])
+
+(#(str %1 " and " %2) "cornbread" "butter beans")
+
+(#(identity %&) 1 "blarg" :yip)
+
+(defn inc-maker
+  "Create custom incrementor"
+  [inc-by]
+  #(+ % inc-by))
+
+(def inc3 (inc-maker 3))
+
+(inc3 7)
