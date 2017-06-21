c05a54d89929f47bcdc9a9c96c38f25e185637fc
Alex Whitt <alex.joseph.whitt@gmail.com>
2017-06-21 08:09:05 -0400

    Finish ch 1

--- clojure-noob/src/clojure_noob/ch-1-exercises.clj
+++ clojure-noob/src/clojure_noob/ch-1-exercises.clj
@@ -7,3 +7,21 @@
 (vector 1 2 3 4)
 
 (list 2 3 4 5)
+
+(hash-map :a :b :c :D)
+
+(hash-set :a :a :b :B :c :d :e)
+
+
+(defn inc100 [x] (+ x 100))
+
+(inc100 5)
+
+(defn dec-maker [decrement]
+  (fn [x] (- x decrement)))
+
+(defn dec50 [x] ((dec-maker 50) x))
+
+(def dec50 (dec-maker 50))
+
+(dec50 100)
